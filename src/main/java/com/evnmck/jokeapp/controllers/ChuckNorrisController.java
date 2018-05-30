e6268aaf963fd90497410c48acb99056ba8e6b58
Evan McClaugherty <evan.mcclaugherty@gmail.com>
2018-05-29 23:20:43 -0600

    Fix @Component to @Controller! yay.

--- src/main/java/com/evnmck/jokeapp/controllers/ChuckNorrisController.java
+++ src/main/java/com/evnmck/jokeapp/controllers/ChuckNorrisController.java
@@ -1,14 +1,16 @@
 package com.evnmck.jokeapp.controllers;
 
 import com.evnmck.jokeapp.services.JokeService;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Component
+@Controller
 public class ChuckNorrisController {
     private JokeService jokeService;
 
+    @Autowired
     public ChuckNorrisController(JokeService jokeService) {
         this.jokeService = jokeService;
     }
